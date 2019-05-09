b83f168fb0f3049e49f1e6db189fac6d48a4df3d
浅野八雲 <asanoyakumo@asanoyakumonoMacBook-Pro.local>
2019-05-09 12:06:45 -0700

    Yakumo 5/9 12:07

---
+++ src/component/setupPage/signUpNameAndPassword.js
@@ -0,0 +1,106 @@
+import React, { Component } from 'react';
+import { TouchableHighlight, Alert, ScrollView, ImageBackground, Image, Text, View, AppRegistry, StyleSheet, Button, TextInput } from 'react-native';
+
+export default class NameAndPassword extends Component{
+  render(){
+    return(
+      <View>
+        <Text style={styles.title1}>
+          NAME AND PASSWORD
+        </Text>
+
+        <TextInput
+          style={styles.textInput}
+          placeholder="Full Name"
+          returnKeyLabel = {"next"}
+          onChangeText={(text) => this.setState({fullname: text})}/>
+
+        <TextInput
+          style={styles.textInput}
+          placeholder="Password"
+          secureTextEntry={true}
+          returnKeyLabel = {"next"}
+          onChangeText={(text) => this.setState({username: text})}/>
+
+        <Text style={styles.text1}>
+          Passwords must include at least 6 characters
+        </Text>
+
+        <Text style={styles.text2}>
+          Save Password
+        </Text>
+
+        <TouchableHighlight style={[styles.buttonContainer, styles.loginButton]} onPress={() => this.onClickListener('next')}>
+          <Text style={styles.loginText}>Continue and Sync Contacts</Text>
+        </TouchableHighlight>
+
+        <TouchableHighlight style={styles.registerPage} onPress={() => this.onClickListener('username')}>
+          <Text style={styles.button1}> Continue without Syncning Contacts</Text>
+        </TouchableHighlight>
+
+      </View>
+    );
+  }
+}
+
+const styles=StyleSheet.create({
+  title1: {
+    fontSize: 20,
+    top: 200,
+    textAlign: 'center',
+    color: 'black',
+  },
+
+  textInput: {
+    top: 300,
+    marginLeft: 60,
+    marginRight: 60,
+    marginBottom: 40,
+    borderBottomWidth: 1,
+  },
+
+  text1: {
+    fontSize: 10,
+    top: 270,
+    color: 'red',
+    marginLeft: 60,
+    fontFamily: 'Avenir',
+  },
+
+  text2: {
+    fontSize: 10,
+    top: 280,
+    color: '#C9C9C9',
+    marginLeft: 100,
+  },
+
+  buttonContainer: {
+    height:45,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom:20,
+    width:250,
+    borderRadius:30,
+    top: 400,
+    marginLeft: 60,
+  },
+
+  loginButton: {
+    backgroundColor: '#00CCFF',
+  },
+
+  loginText: {
+    color: 'white',
+  },
+
+  registerPage: {
+    top: 400,
+    alignItems: 'center',
+  },
+
+  button1: {
+    color: '#00CCFF',
+  },
+})
+AppRegistry.registerComponent('project1', () => NameAndPassword );
