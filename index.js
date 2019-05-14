6508f419abdec99c14c1479ddb8c01a4f9a035ca
浅野八雲 <asanoyakumo@asanoyakumonoMacBook-Pro.local>
2019-05-14 15:43:37 -0700

    Yakumo 5/14 15:43

--- index.js
+++ index.js
@@ -1,111 +1,39 @@
-import React from 'react';
-import { View, AppRegistry } from 'react-native';
-import App from './src/App';
+import React, { Component } from 'react';
+import { ScrollView, Text, View, AppRegistry, StyleSheet } from 'react-native';
 
-<<<<<<< HEAD
-export default class NameAndPassword extends Component{
+const styles=StyleSheet.create({
+  title: {
+    flex: 1,
+    fontSize: 20,
+    color: 'black',
+    alignItems: 'center',
+    paddingTop: 100,
+  },
+  explanation: {
+    flex: 1,
+    flexDirection: 'row',
+    fontSize: 10,
+    color: 'black',
+    margin: 20,
+  },
+})
+
+export default class UseOfTerms extends Component{
   render(){
     return(
-      <View>
-        <Text style={styles.title1}>
-          NAME AND PASSWORD
-        </Text>
-
-        <TextInput
-          style={styles.textInput}
-          placeholder="Full Name"
-          returnKeyLabel = {"next"}
-          onChangeText={(text) => this.setState({fullname: text})}/>
-
-        <TextInput
-          style={styles.textInput}
-          placeholder="Password"
-          secureTextEntry={true}
-          returnKeyLabel = {"next"}
-          onChangeText={(text) => this.setState({username: text})}/>
-
-        <Text style={styles.text1}>
-          Passwords must include at least 6 characters
-        </Text>
-
-        <Text style={styles.text2}>
-          Save Password
-        </Text>
-
-        <TouchableHighlight style={[styles.buttonContainer, styles.loginButton]} onPress={() => this.onClickListener('next')}>
-          <Text style={styles.loginText}>Continue and Sync Contacts</Text>
-        </TouchableHighlight>
-
-        <TouchableHighlight style={styles.registerPage} onPress={() => this.onClickListener('username')}>
-          <Text style={styles.button1}> Continue without Syncning Contacts</Text>
-        </TouchableHighlight>
-
-      </View>
+        <ScrollView style={{flex: 1, padding: 10}}>
+          <View style={{alignItems: 'center'}}>
+            <Text style = {styles.title}> Use of Terms </Text>
+              <View>
+                <Text style = {styles.explanation}> 1. FORISをご利用頂き、誠に有難うございます。FORISでは個人情報の管理をMicrosoft社のAzureを使用しております。
+                個人情報に関しまして、厳重な管理を務めております.
+                </Text>
+              </View>
+          </View>
+        </ScrollView>
     );
   }
 }
 
-const styles=StyleSheet.create({
-  title1: {
-    fontSize: 20,
-    top: 200,
-    textAlign: 'center',
-    color: 'black',
-  },
 
-  textInput: {
-    top: 300,
-    marginLeft: 60,
-    marginRight: 60,
-    marginBottom: 40,
-    borderBottomWidth: 1,
-  },
-
-  text1: {
-    fontSize: 10,
-    top: 270,
-    color: 'red',
-    marginLeft: 60,
-    fontFamily: 'Avenir',
-  },
-
-  text2: {
-    fontSize: 10,
-    top: 280,
-    color: '#C9C9C9',
-    marginLeft: 100,
-  },
-
-  buttonContainer: {
-    height:45,
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginBottom:20,
-    width:250,
-    borderRadius:30,
-    top: 400,
-    marginLeft: 60,
-  },
-
-  loginButton: {
-    backgroundColor: '#00CCFF',
-  },
-
-  loginText: {
-    color: 'white',
-  },
-
-  registerPage: {
-    top: 400,
-    alignItems: 'center',
-  },
-
-  button1: {
-    color: '#00CCFF',
-  },
-})
-AppRegistry.registerComponent('project1', () => NameAndPassword );
-=======
-AppRegistry.registerComponent('project1', () => App );
->>>>>>> 0df1de6aad07d4b0cb102c13a9206cadb5ed9be8
+AppRegistry.registerComponent('project1', () => UseOfTerms);
